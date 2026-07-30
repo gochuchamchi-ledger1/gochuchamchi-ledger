d4ca6441ab4b856b5e553e7b5ec09f945baf3f82
gochuchamchi-ledger1 <luxurysjx@gmail.com>
2026-07-30 22:36:56 +0900

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,9 +1,5 @@
-const CACHE='gochuchamchi-v700-admin-pin-report';
+const CACHE='gochuchamchi-v710-pdf-report-large-avatar';
 const ASSETS=["./", "./index.html", "./manifest.webmanifest", "./icon-192.png", "./icon-512.png", "./mascot.png", "./home-cat.png", "./transaction-cat.png", "./member-cat.png", "./report-cat.png", "./arrears-cat.png", "./report-qr.png"];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
-self.addEventListener('fetch',e=>{
- if(e.request.method!=='GET')return;
- e.respondWith(fetch(e.request).then(r=>{const copy=r.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return r;})
- .catch(()=>caches.match(e.request).then(r=>r||caches.match('./index.html'))));
-});
+self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;e.respondWith(fetch(e.request).then(r=>{const cp=r.clone();caches.open(CACHE).then(c=>c.put(e.request,cp));return r}).catch(()=>caches.match(e.request).then(r=>r||caches.match('./index.html'))))});
