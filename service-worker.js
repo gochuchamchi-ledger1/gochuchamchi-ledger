199100ad45abf511176027d5b8fe37b1747f24ba
gochuchamchi-ledger1 <luxurysjx@gmail.com>
2026-07-30 16:28:28 +0900

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE='gochuchamchi-v54-auto-close-avatar';
+const CACHE='gochuchamchi-v541-runtime-stable';
 const CORE=[
  './','./index.html','./manifest.webmanifest',
  './icon-192.png','./icon-512.png','./mascot.png',
