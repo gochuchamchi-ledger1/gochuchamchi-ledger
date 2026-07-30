939a7e94921cd1ae6dda3d6055702e705d978ef2
gochuchamchi-ledger1 <luxurysjx@gmail.com>
2026-07-30 19:13:26 +0900

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,1 +1,10 @@
-const CACHE='gochuchamchi-v600-firebase-realtime';const ASSETS=['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png'];self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(k=>Promise.all(k.filter(x=>x!==CACHE).map(x=>caches.delete(x))))));self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;e.respondWith(fetch(e.request).catch(()=>caches.match(e.request)))})
+const CACHE='gochuchamchi-v601-install-share';
+const ASSETS=["./", "./index.html", "./manifest.webmanifest", "./icon-192.png", "./icon-512.png", "./mascot.png", "./home-cat.png", "./transaction-cat.png", "./member-cat.png", "./report-cat.png", "./arrears-cat.png", "./report-qr.png"];
+self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
+self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
+self.addEventListener('fetch',e=>{
+ if(e.request.method!=='GET')return;
+ e.respondWith(fetch(e.request).then(r=>{
+  const copy=r.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return r;
+ }).catch(()=>caches.match(e.request).then(r=>r||caches.match('./index.html'))));
+});
