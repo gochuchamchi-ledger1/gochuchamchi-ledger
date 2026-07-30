9794b118b8023254a7541986b4864266d813cab6
gochuchamchi-ledger1 <luxurysjx@gmail.com>
2026-07-30 19:23:18 +0900

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE='gochuchamchi-v601-install-share';
+const CACHE='gochuchamchi-v610-auth-role';
 const ASSETS=["./", "./index.html", "./manifest.webmanifest", "./icon-192.png", "./icon-512.png", "./mascot.png", "./home-cat.png", "./transaction-cat.png", "./member-cat.png", "./report-cat.png", "./arrears-cat.png", "./report-qr.png"];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
