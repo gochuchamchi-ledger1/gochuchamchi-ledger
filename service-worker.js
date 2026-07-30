ce8c989aba6f8f0955c8b0a56aa65af633618566
gochuchamchi-ledger1 <luxurysjx@gmail.com>
2026-07-30 14:46:19 +0900

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,13 +1,29 @@
-const CACHE='gochuchamchi-v4-import-preview';
-const CORE=['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png','./mascot.png','./home-cat.png','./transaction-cat.png','./member-cat.png','./report-cat.png','./arrears-cat.png'];
-self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE)))});
-self.addEventListener('activate',e=>{e.waitUntil(Promise.all([self.clients.claim(),caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==CACHE).map(k=>caches.delete(k))))]))});
+const CACHE='gochuchamchi-v5-reports';
+const CORE=[
+ './','./index.html','./manifest.webmanifest',
+ './icon-192.png','./icon-512.png','./mascot.png',
+ './home-cat.png','./transaction-cat.png','./member-cat.png',
+ './report-cat.png','./arrears-cat.png'
+];
+self.addEventListener('install',e=>{
+ self.skipWaiting();
+ e.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE)));
+});
+self.addEventListener('activate',e=>{
+ e.waitUntil(Promise.all([
+  self.clients.claim(),
+  caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))
+ ]));
+});
 self.addEventListener('fetch',e=>{
  if(e.request.method!=='GET')return;
- e.respondWith(caches.match(e.request).then(hit=>hit||fetch(e.request).then(r=>{
-   if(r && r.status===200 && new URL(e.request.url).origin===location.origin){
-     const cp=r.clone();caches.open(CACHE).then(c=>c.put(e.request,cp));
+ e.respondWith(
+  fetch(e.request).then(resp=>{
+   if(resp && resp.status===200 && new URL(e.request.url).origin===location.origin){
+    const copy=resp.clone();
+    caches.open(CACHE).then(c=>c.put(e.request,copy));
    }
-   return r;
- }).catch(()=>e.request.mode==='navigate'?caches.match('./index.html'):undefined)));
+   return resp;
+  }).catch(()=>caches.match(e.request).then(hit=>hit||(e.request.mode==='navigate'?caches.match('./index.html'):undefined)))
+ );
 });
